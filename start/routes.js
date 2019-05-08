ae81ca1b70e8f027d50f6fe7be3d188a2ccb80da
Alessandrode Sousa Rocha <alessandro.s.rocha@hotmail.com>
2019-05-08 16:14:30 -0300

    Primary commit

---
+++ start/routes.js
@@ -0,0 +1,15 @@
+'use strict'
+
+
+/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
+
+
+const Route = use('Route')
+
+Route.get('/users','UserController.js')
+Route.get('/sessions','SessionController.js')
+
+
+Route.resource('properties','PropertyController')
+    .apiOnly()     //method get and set have no routes
+    .middleware('auht') //only logged in users can access
